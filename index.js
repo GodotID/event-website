cbd0b3d20aff5a2beae7d7094f6ac0ca66f1d2c4
Hanz <hanz@godot.id>
2022-10-31 12:36:58 +0700

    feat: Added frontend

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public/dist/'));
+
 if (!process.env.DETA_RUNTIME) {
 	app.listen(8080, console.log.bind(globalThis, '[i] Ran at 8080'));
 } else {
